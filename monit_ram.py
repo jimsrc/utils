6d299f3a2e0c7ba01c481afad49276dc14fa2d74
jimlamp <jimmy.ilws@gmail.com>
2017-07-27 15:05:21 -0300

    now we moved some functions to a funcs.py library

--- monit_ram.py
+++ monit_ram.py
@@ -4,11 +4,7 @@ import psutil, time
 import argparse, sys
 import numpy as np
 from datetime import datetime, timedelta
-
-
-def utc2date(utcsec):
-    utcini = datetime(1970,1,1,0,0)
-    return utcini + timedelta(seconds=utcsec)
+import funcs as ff
 
 
 def measure(start, now):
@@ -17,29 +13,6 @@ def measure(start, now):
     else:
         return now > start
 
-
-def build_measure_array(t, *data):
-    buff = np.array([t, data[0]]).T
-    if len(data)==1:
-        # we are done.
-        return buff
-   
-    # iterate from the 2nd element
-    for _data in data[1:]:
-        data_elem = np.array(_data).reshape(len(_data),1)
-        buff = np.concatenate([buff, data_elem], axis=1)
-    return buff
-
-
-def save_measure(fname, start, t, *data):
-    buff = build_measure_array(t, *data)
-    timestmp = utc2date(start).strftime('%d %b %Y %H:%M:%S')
-    HEADER = """
-    Memory consumption measurements.
-    Measurement started at {timestmp} (UTC)
-    """.format(timestmp=timestmp)
-    np.savetxt(fname, X=buff, fmt='%g', header=HEADER)
-    
 
 def main():
     #--- retrieve args
@@ -60,7 +33,7 @@ def main():
     mem_tot = psutil.virtual_memory()[0] # [bytes]
 
     start = time.time() # mediante argparse, cambiarle el valor inicial
-    print('\n [*] Starting measurement at ' + utc2date(start).strftime('%d %b %Y %H:%M:%S') + ' (UTC) \n')
+    print('\n [*] Starting measurement at ' + ff.utc2date(start).strftime('%d %b %Y %H:%M:%S') + ' (UTC) \n')
     #while measure(start, time.time()):
     while True:
         try:
@@ -75,7 +48,7 @@ def main():
 
         except KeyboardInterrupt:
             print("\n [*] saving measurements...\n")
-            save_measure(pa.fname_out, start, t, mem_used2, mem_used)
+            ff.save_measure(pa.fname_out, start, t, mem_used2, mem_used)
             print('\n [+] success on saving:\n %s\n'%pa.fname_out)
             break
 
