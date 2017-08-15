c6544a59546b7930f9e2085fc926bcce6205032d
jimlamp <jimmy.ilws@gmail.com>
2017-08-15 14:31:50 -0300

    [ram] more verbose output && better code

--- calc_mem.py
+++ calc_mem.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env ipython
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import numpy as np
 import argparse
@@ -30,22 +30,35 @@ help='value of GRIDLEVELs (for AMR).',
 pa = parser.parse_args()
 
 
-scell   = 3.2   # [Kbytes, KB] aprox.
+# asumiendo q los datos son floats de 4bytes:
+scell   = 3.2*2.   # [Kbytes, KB] aprox. size of each cell
 
 ncx, ncy, ncz   = pa.cells #4, 4, 4
-blk_size_cells  = ncx*ncy*ncz  # [nmbr of cells]
-blk_size        = 1.0*blk_size_cells*scell  # [KB]
-blk_size_tot    = 1.0*(ncx+4)*(ncy+4)*(ncz+4)*scell # [KB]
+blk_size        = 1.0*ncx*ncy*ncz*scell  # [KB] block size taking computational cells only, for each block
+blk_size_tot    = 1.0*(ncx+4)*(ncy+4)*(ncz+4)*scell # [KB] block size taking total nmbr of cells, for each block
 
 # nmbr of Root Blocks (in each dimension)
 nbx, nby, nbz   = pa.blocks #2, 2, 2
 
 size_comp       = 1.0*nbx*nby*nbz * blk_size    # [KB]
+size_comp_max   = 1.0*size_comp*(8**(pa.glevel))   # [KB]
 size_tot        = 1.0*nbx*nby*nbz * blk_size_tot
+size_tot_max    = size_tot*(8**(pa.glevel))   # [KB]
 
-print " mem computational: %g MB" % (size_comp/(1024.))
-print " mem total: %g MB " % (size_tot/(1024.))
-print " max total: %g MB " % (size_tot*(2**pa.glevel-1)/(1024.))
+print " nmbr of computational blocks                : %d" % (nbx*nby*nbz)
+print " nmbr of computational blocks (w/ max refine): %d" % (nbx*nby*nbz * (8**(pa.glevel)))
+print " nmbr of computationsl cells                 : %d" % (nbx*nby*nbz * ncx*ncy*ncz)
+print " nmbr of computationsl cells (w/ max refine) : %d\n" % (nbx*nby*nbz * ncx*ncy*ncz *(8**(pa.glevel)))
+
+print " total nmbr of cells (comp+ghost)            : %d" % (nbx*nby*nbz * (ncx+4)*(ncy+4)*(ncz+4))
+print " total nmbr of cells (w/ max refine)         : %d" % (nbx*nby*nbz * (ncx+4)*(ncy+4)*(ncz+4) * (8**(pa.glevel)))
+print ""
+
+
+print " mem computational     : %g MB" % (size_comp/(1024.))
+print " mem computational MAX : %g MB" % (size_comp_max/(1024.))
+print " mem total             : %g MB " % (size_tot/(1024.))
+print " mem total MAX         : %g MB " % (size_tot_max/(1024.))
 
 
 #EOF
